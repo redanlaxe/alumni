73b1866edebf9a9e74f78198c575c95dc4c4e400
Declin <dayvid.kayal@gmail.com>
2013-06-24 16:43:21 +0200

    Upgrade to 2.2.3 Grails

--- grails-app/controllers/LogoutController.groovy
+++ grails-app/controllers/LogoutController.groovy
@@ -7,6 +7,7 @@ class LogoutController {
 	 */
 	def index = {
 		// TODO put any pre-logout code here
+                principal = null
 		redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl // '/j_spring_security_logout'
 	}
 }
