408fbcb0d080329ab29dac510573af4c4c8373ed
Declin <dayvid.kayal@gmail.com>
2013-06-24 15:40:19 +0200

    Création d'un utilisateur par défaut dans le BootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,33 @@
+import com.miage.alumni.*
+
 class BootStrap {
 
     def init = { servletContext ->
+        
+        def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError:true)
+        def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError:true)
+        
+        def user0 = Personne.findByUsername('user0') ?: new Personne(username: 'user0', enabled: true, password:'pass', firstName: 'first', lastName: 'user').save(failOnError:true)
+        if(!user0.authorities.contains(userRole))
+        {
+            PersonneRole.create user0, userRole, true
+        }
+        
+        
+        def user1 = Personne.findByUsername('day') ?: new Personne(username: 'day', enabled: true, password:'pass', firstName: 'day', lastName: 'Ka').save(failOnError:true)
+        if(!user1.authorities.contains(userRole))
+        {
+            PersonneRole.create user1, userRole, true
+        }
+        if(!user1.authorities.contains(adminRole))
+        {
+            PersonneRole.create user1, adminRole, true
+        }
+        
+        assert Personne.count() == 2
+        assert Role.count() == 2
+        assert PersonneRole.count() == 2
     }
-    def destroy = {
-    }
+    
+    def destroy = {}
 }
